afa91eb8478f747d1f5902a9ced4b50fe95b2b17
Sudeep-tiwari10 <sudeep.tiwari68@law.ac.uk>
2025-01-14 12:21:22 +0000

    Unit 1 - Task 1, Print "Hello World"

---
+++ Unit1-task1.py
@@ -0,0 +1,7 @@
+# An exemplar solution for Consolidate Task 2: Beyond Hello World
+
+print("Welcome to the Software Development unit")
+
+print("Hello, my name is Sudeep Tiwari.")
+
+print("Learning Python is easy, and I am really enjoying it!")
